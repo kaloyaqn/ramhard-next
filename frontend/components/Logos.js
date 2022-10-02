a9a57e570c88005c7964d549edeb50409d3749d7
kaloyaqn <kallnoob11@gmail.com>
2022-10-02 18:48:21 +0300

    Co-authored-by: BojidarC <BojidarC@users.noreply.github.com>

--- frontend/components/Logos.js
+++ frontend/components/Logos.js
@@ -1,15 +1,31 @@
 import Script from 'next/script'
 
+/**
+ * 
+ * 
+ *  <section className='logos slider'>
+            <div className='container'>
+              <div className='row'>
+                <div className='logo-placeholder'>
+                <div class="slide"><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"/></div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+ */
+
 export default function Logos() {
     return (
         <>
-          <section className='logos'>
+        <Script>        
+        </Script>
+          <section className='logo-carousel slide'>
             <div className='container'>
               <div className='row'>
-                <div className='logo-placeholder'>Logos Here</div>
-                  <div class="slide">
-
-                  </div>
+                <div className='logo-placeholder'>
+                  <div class="slide"><img src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png" /></div>
+                </div>
               </div>
             </div>
           </section>
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -19,13 +19,17 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="icon" href="/favicon.ico" />
+        <Script src="./components/Logos_script.js"></Script>
       </Head>
 
       <Navbar />
 
       <Component {...pageProps} />
 
+      
       <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></Script>
     </>
   );
 }
