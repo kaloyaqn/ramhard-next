0d3fa3cd76638aad4d9db7efaf0449930da25e52
BojidarC <114111045+BojidarC@users.noreply.github.com>
2022-10-12 20:05:02 +0300

    s

--- frontend/pages/client.js
+++ frontend/pages/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: "bc5owe4x",
     dataset: 'production',
     apiVersion: '2022-03-10',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: "skWbipqk01RrdAVqfGVnv4WyakOj412DhXs1h1TyC3fYkS8CD6wVavP2EfbggEx0sF0dERq0KHK964D1ge27HFq3cnsZvArTs7k07M8X6poz1y8bOIle5aod1vu91wW8H0mP1ymyvGMKL8Zq9yCLcQjfNPE1diuS6x8rMjmV4JbpqRpVe7Op",
 });
 
 const builder = imageUrlBuilder(client);
